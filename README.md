660b64a18594988d9909332623d70f86c528b26a
Ryan Chew <me@ryan.moe>
2019-10-17 05:23:59 +1100

    Refactored

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ This is a tool for creating a basic rest api with your own name and fields.
 Usage:
 
 `python3 automate.py <file-extension> <name-of-route-and-model> <field1> <field2> ...`
+
+Example use:
+
+Making a basic REST model/route named 'transactions' with the fields: 'id', 'name', 'company'
+using expressjs 
+
+`python3 automate.py js transactions id name company`
+
+Currently supported languages/frameworks are:
+
+* javascript/expressjs
--- automate.py
+++ automate.py
@@ -18,58 +18,34 @@ def main():
 
     language = sys.argv[1]
     name_of_api = sys.argv[2]
-
-
-    if name_of_api == "users":
-        routes_file = open("./sources/routes.user.txt", "r")
-        models_file = open("./sources/models.user.txt", "r")
-
-        models_file_out = open("./userModel.js", "w")
-        user_data = sys.argv[3:]
-
-        models_data_array = []
-
-        for s in user_data:
-            new_block = models_block.replace("%", s)
-            models_data_array.append(new_block)
-
-        for line in models_file:
-            if "%" in line:
-                line = models_data_processing(models_data_array)
-
-            models_file_out.write(line)
-
-        models_file_out.close()
-
-    else:
-        routes_file = open("./sources/routes." + language , "r")
-        models_file = open("./sources/models." + language, "r")
-
-        routes_file_out = open("./" + name_of_api + "." + language, "w")
-        models_file_out = open("./" + name_of_api + "Model." + language, "w")
-
-        data = sys.argv[3:]
-
-        models_data_array = []
-        routes_data_array = []
-
-        for s in data:
-            new_block = models_block.replace("%", s)
-            models_data_array.append(new_block)
-
-        for s in data:
-            new_block = routes_block.replace("%", s)
-            routes_data_array.append(new_block)
-
-        writeout (models_file, False, models_data_array, name_of_api, models_file_out)
-        writeout (routes_file, True, routes_data_array, name_of_api, routes_file_out)
-
-        models_file_out.close()
-        routes_file_out.close()
-        routes_file.close()
-        models_file.close()
-
     
+    routes_file = open("./sources/routes." + language , "r")
+    models_file = open("./sources/models." + language, "r")
+
+    routes_file_out = open("./" + name_of_api + "." + language, "w")
+    models_file_out = open("./" + name_of_api + "Model." + language, "w")
+
+    data = sys.argv[3:]
+
+    models_data_array = []
+    routes_data_array = []
+
+    process_data_array (data, models_data_array, False)
+    process_data_array (data, routes_data_array, True)
+
+    writeout (models_file, False, models_data_array, name_of_api, models_file_out)
+    writeout (routes_file, True, routes_data_array, name_of_api, routes_file_out)
+
+    models_file_out.close()
+    routes_file_out.close()
+    routes_file.close()
+    models_file.close()
+
+
+def process_data_array(data, data_array, rORm):
+    for s in data:
+        new_block = routes_block.replace("%", s) if rORm else models_block.replace("%", s)
+        data_array.append(new_block)
 
 
 def writeout (OGfile, rORm, data_array, name, outFile):
